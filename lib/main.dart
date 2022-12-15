9850e91090aa387798b5ae1a3d6b6d0e55a8b405
Robert Zanceanu <robert.zanceanu@ext.getpenta.com>
2022-12-15 16:32:20 +0200

    Add all header elements

--- lib/main.dart
+++ lib/main.dart
@@ -79,14 +79,31 @@ class _MyHomePageState extends State<MyHomePage> {
           child: Column(
         children: [
           Padding(
-            padding: EdgeInsets.only(left: 24, right: 24, top: 30, bottom: 30),
+            padding:
+                const EdgeInsets.only(left: 24, right: 24, top: 30, bottom: 30),
             child: Row(
               children: [
                 Text('devfinder',
                     style: Theme.of(context)
                         .textTheme
                         .headline1
-                        ?.apply(color: color_constants.neutral02Color))
+                        ?.apply(color: color_constants.neutral02Color)),
+                const Spacer(),
+                Container(
+                    margin: const EdgeInsets.only(
+                      right: 16,
+                    ),
+                    child: const Text('DARK',
+                        style: TextStyle(
+                            fontSize: 13,
+                            color: color_constants.tertiaryColor,
+                            fontWeight: FontWeight.bold,
+                            letterSpacing: 2.5))),
+                const Icon(
+                  Icons.nights_stay_sharp,
+                  color: color_constants.tertiaryColor,
+                  size: 20,
+                )
               ],
             ),
           )
