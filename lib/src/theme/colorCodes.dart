9a2125ae848a97f32f9b642b3e4fe5912c5ba3bb
Dan Diac <danut.diac@thinslices.com>
2022-12-15 18:28:31 +0200

    Add two grey colors constants

--- lib/src/theme/colorCodes.dart
+++ lib/src/theme/colorCodes.dart
@@ -5,3 +5,5 @@ const Color secondaryColor = Color(0xff697C9A);
 const Color tertiaryColor = Color(0xff4B6A9B);
 const Color neutral01Color = Color(0xff2B3442);
 const Color neutral02Color = Color(0xff222731);
+const Color lightGrey = Color.fromRGBO(242, 242, 242, 1);
+const Color darkGrey = Color.fromRGBO(20, 29, 47, 1);
