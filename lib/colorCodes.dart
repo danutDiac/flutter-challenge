7c7da5d54e5f486e6b282fdfcd4c574f03ee17cf
Robert Zanceanu <robert.zanceanu@ext.getpenta.com>
2022-12-15 14:15:23 +0200

    Add global color constants

---
+++ lib/colorCodes.dart
@@ -0,0 +1,6 @@
+import 'dart:ui';
+
+const Color primaryColor = Color(0xff006de6);
+const Color secondaryColor = Color(0xff697C9A);
+const Color tertiaryColor = Color(0xff4B6A9B);
+const Color neutral01Color = Color(0xff2B3442);
