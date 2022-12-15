7a7e5bf0ef25fb711f8486b44b9ee7d3fc41d72d
Robert Zanceanu <robert.zanceanu@ext.getpenta.com>
2022-12-15 16:18:26 +0200

    Implement first draft of header

--- lib/src/theme/colorCodes.dart
+++ lib/src/theme/colorCodes.dart
@@ -4,3 +4,4 @@ const Color primaryColor = Color(0xff006de6);
 const Color secondaryColor = Color(0xff697C9A);
 const Color tertiaryColor = Color(0xff4B6A9B);
 const Color neutral01Color = Color(0xff2B3442);
+const Color neutral02Color = Color(0xff222731);
