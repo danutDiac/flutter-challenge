3476b9afd038478a1c75c01cb971ae57670aecc5
Robert Zanceanu <robert.zanceanu@ext.getpenta.com>
2022-12-15 14:15:12 +0200

    Add custom palette for swatch

---
+++ lib/palette.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Palette {
+  static const MaterialColor kToDark = MaterialColor(
+    0x00ff79ff,
+    <int, Color>{
+      50: Color(0xff006de6), //10%
+      100: Color(0xff0061cc), //20%
+      200: Color(0xff0055b3), //30%
+      300: Color(0xff004999), //40%
+      400: Color(0xff003d80), //50%
+      500: Color(0xff003066), //60%
+      600: Color(0xff00244c), //70%
+      700: Color(0xff001833), //80%
+      800: Color(0xff000c19), //90%
+      900: Color(0xff000000), //100%
+    },
+  );
+}
