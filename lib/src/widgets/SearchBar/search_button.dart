d360acede5c1fb43469232949ca25be10479632d
Dan Diac <danut.diac@thinslices.com>
2022-12-20 09:08:10 +0200

    Enable onPress background color change for search button

--- lib/src/widgets/SearchBar/search_button.dart
+++ lib/src/widgets/SearchBar/search_button.dart
@@ -13,13 +13,22 @@ class SearchButton extends StatelessWidget {
       margin: const EdgeInsets.all(7),
       child: TextButton(
         onPressed: isDisabled ? null : () => log('Searched!'),
-        style: TextButton.styleFrom(
-            disabledBackgroundColor: color_constants.lightBlue,
-            backgroundColor: color_constants.primary02Color,
-            padding: const EdgeInsets.only(
-                top: 12.5, bottom: 12.5, left: 18, right: 18),
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10))),
+        style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.resolveWith<Color>(
+              (Set<MaterialState> states) {
+                if (isDisabled) {
+                  return color_constants.lightBlue;
+                }
+                if (states.contains(MaterialState.pressed)) {
+                  return color_constants.lightBlue;
+                }
+                return color_constants.primary02Color;
+              },
+            ),
+            padding: MaterialStateProperty.all(const EdgeInsets.only(
+                top: 12.5, bottom: 12.5, left: 18, right: 18)),
+            shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10)))),
         child: const Text(
           'Search',
           style: TextStyle(
