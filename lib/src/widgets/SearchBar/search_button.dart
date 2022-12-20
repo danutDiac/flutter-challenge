d03a23384e67a9720d06ac55df493bef0c19d18d
Robert Zanceanu <robert.zanceanu@ext.getpenta.com>
2022-12-20 09:27:57 +0200

    Add onSearch functionality for search button

--- lib/src/widgets/SearchBar/search_button.dart
+++ lib/src/widgets/SearchBar/search_button.dart
@@ -4,14 +4,15 @@ import 'package:flutter/material.dart';
 import '../../theme/color_codes.dart' as color_constants;
 
 class SearchButton extends StatelessWidget {
-  const SearchButton({super.key});
+  const SearchButton({super.key, required this.onSearch});
+  final Function() onSearch;
 
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: const EdgeInsets.all(7),
       child: TextButton(
-        onPressed: () => log('Searched!'),
+        onPressed: onSearch,
         style: TextButton.styleFrom(
             backgroundColor: color_constants.primary02Color,
             padding: const EdgeInsets.only(
